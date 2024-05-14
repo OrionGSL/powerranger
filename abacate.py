b4e1d42e6a8fbb47cbf613ea40c6eacd65a1f26a
OrionGSL <128553861+OrionGSL@users.noreply.github.com>
2024-05-13 21:05:02 -0300

    Add files via upload

---
+++ abacate.py
@@ -0,0 +1,7 @@
+numberOne = float(input('Digite o Primeironúmero: '))
+equacion = input('Digite a equação: ( = - / * )')
+numberTwo = float(input('Digite o segundo número: '))
+
+if equacion == '+':
+    result = numberOne + numberTwo
+    print(result)
